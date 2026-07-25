78a75c9ec7aef0e9d8de141173fc75394d532c64
PaulaFreitas360 <120344651+PaulaFreitas360@users.noreply.github.com>
2026-07-25 17:02:22 -0300

    Adicionar src/states/index.js

---
+++ src/states/index.js
@@ -0,0 +1,23 @@
+/**
+ * states/index.js
+ * Exportador de todos os 15 state handlers
+ */
+
+module.exports = {
+  0: require('./state-0'),
+  1: require('./state-1'),
+  2: require('./state-2'),
+  3: require('./state-3'),
+  4: require('./state-4'),
+  5: require('./state-5'),
+  6: require('./state-6'),
+  7: require('./state-7'),
+  8: require('./state-8'),
+  9: require('./state-9'),
+  10: require('./state-10'),
+  11: require('./state-11'),
+  12: require('./state-12'),
+  13: require('./state-13'),
+  14: require('./state-14'),
+  15: require('./state-15'),
+};
